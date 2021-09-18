1d0a157e570d228db7dea28d2f7a52a52b9ddbe5
Andre Luis Moreira <andreluismoreirasmo@gmail.com>
2021-09-17 21:07:08 -0300

    Doc: Ignora console.log na mensagem de inicialiazacao do sistema

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -12,22 +12,21 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-
-    return response.status(500).json({
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
       status: 'error',
-      message: 'Internal Server Error',
+      message: error.message,
     });
-  },
-);
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('Server started on port 3333!');
 });
